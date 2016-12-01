e9b2e57aa627e4c688d7be8b43b5d51345cf1628
Jakub Syrek <jksyrek@gmail.com>
2016-12-02 00:30:43 +0100

    Implement UDP scann

--- scanner.rb
+++ scanner.rb
@@ -10,13 +10,14 @@ class Scanner
     @tries
     @config
 
-  def initialize(ip, dst, src, timeout_value, tries)
+  def initialize(ip, dst, src, timeout_value, tries, sleep_time)
 
     @ip = ip
     @dst = dst
     @src = src
     @timeout_value = timeout_value
     @tries = tries
+    @sleep_time = sleep_time
     @config = PacketFu::Utils.whoami?()
 
     @prepared_packet = prepare
@@ -37,7 +38,7 @@ class Scanner
   end
 
   def additional_config(packet)
-    packet.payload = "TCP sny probe"
+    # packet.payload = "TCP sny probe"   it oculd be error?
     packet.tcp_flags.syn = 1
     packet
   end
@@ -46,19 +47,49 @@ class Scanner
     pkt.is_tcp?
   end
 
-  def check(pkt)
-    pkt.ip_saddr == @ip and pkt.tcp_dport ==  @prepared_packet.tcp_src and pkt.tcp_flags.syn == 1 and pkt.tcp_flags.ack == 1
+  def check_if_icmp(pkt)
+    pkt.is_icmp?
   end
 
-  def on_check_success
-    puts "#{@ip} is up"
+  def check_packet_flags(pkt)
+    pkt.ip_saddr == @ip and pkt.tcp_dst == @prepared_packet.tcp_src and pkt.tcp_src == @dst and pkt.tcp_flags.syn == 1
+  end
+
+  def check_tcp_rst(pkt)
+    pkt.ip_saddr == @ip and pkt.tcp_dst == @prepared_packet.tcp_src and pkt.tcp_src == @dst and pkt.tcp_flags.rst == 1
+  end
+
+  def check_icmp(pkt)
+    pkt.ip_saddr == @ip and pkt.icmp_type == 3 and (pkt.icmp_code == 0 or 1 or 2 or 3 or 9 or 10 or 13)
+  end
+
+
+  def on_icmp_check_success
+    puts "#{@ip} is fitered (icmp received)"
   end
 
   def on_timeout
+    puts "#{@ip} is filtered"
+  end
+
+  def is_successful
+    puts "#{@ip} is up"
+  end
+
+  def is_not_successful
     puts "#{@ip} is down"
   end
 
+  def is_filtered
+    puts "#{@ip} is filtered"
+  end
+
   public
+
+  def set_dst_port(port)
+    @dst = port
+    @prepared_packet = prepare
+  end
 
   def scann
 
@@ -68,10 +99,19 @@ class Scanner
         cap = PacketFu::Capture.new(:iface => @config[:iface], :start => true)
         cap.stream.each do |p|
           pkt = PacketFu::Packet.parse p
-          next unless check_packet_type(pkt)
-          if check(pkt)
-            on_check_success
+          if check_packet_type(pkt)
+            if check_packet_flags(pkt)    #fails if first fials or check second condition?
+              is_successful
+              break
+            elsif check_tcp_rst(pkt)
+              is_not_successful
+              break
+            end
+          elsif check_if_icmp(pkt) and check_icmp(pkt)
+            on_icmp_check_success
             break
+          else
+            next
           end
         end
       }
@@ -85,14 +125,152 @@ class Scanner
   capture_thread.join
   end
 
+end
+
+class FIN_scanner < Scanner
+
+  def additional_config(packet)
+    # packet.payload = "TCP sny probe"
+    packet.tcp_flags.fin = 1
+    packet
+  end
+
+  def check_packet_flags(pkt)
+    pkt.ip_saddr == @ip and pkt.tcp_dst == @prepared_packet.tcp_src and pkt.tcp_src == @dst and pkt.tcp_flags.rst == 1
+  end
+
+  # when responded with rst port is down
+  def is_successful
+    puts "#{@ip} is down"
+  end
+
+  def on_timeout
+    puts "#{@ip} is up"
+  end
 
 end
 
-ip = "192.168.0.2"
-dst = 80
-src = 999
-timeout_value = 3
-tries = 10
-scanner = Scanner.new(ip, dst, src, timeout_value, tries)
+class SYN_scanner < Scanner
 
-scanner.scann
+end
+
+class ACK_scanner < Scanner
+
+  def additional_config(packet)
+    # packet.payload = "TCP sny probe"
+    packet.tcp_flags.ack = 1
+    packet
+  end
+
+  def check_packet_flags(pkt)
+    pkt.ip_saddr == @ip and pkt.tcp_dst == @prepared_packet.tcp_src and pkt.tcp_src == @dst and pkt.tcp_flags.rst == 1
+  end
+
+  # when responded with rst port is down
+  def is_successful
+    puts "#{@ip} is unfiltered"
+  end
+
+  def on_timeout
+    puts "#{@ip} is filtered"
+  end
+
+  def on_icmp_check_success
+    puts "#{@ip} is filtered"
+  end
+
+end
+
+class UDP_scanner < Scanner
+
+  def prepare
+
+    packet = PacketFu::UDPPacket.new(:config => @config)
+    packet.ip_daddr = @ip
+    packet.udp_dst = @dst
+    packet.udp_src = @src
+
+    packet.recalc
+
+    packet
+  end
+
+  def check_packet_type(pkt)
+    pkt.is_udp? and pkt.ip_saddr == @ip
+  end
+
+  def check_if_icmp(pkt)
+    pkt.is_icmp?
+  end
+
+  def on_timeout
+    puts "#{@ip} is open|filtered"
+  end
+
+  def is_successful
+    puts "#{@ip} is up"
+  end
+
+  def is_not_successful
+    puts "#{@ip} is down"
+  end
+
+  public
+
+  def scann
+
+    capture_thread = Thread.new do
+      begin
+        Timeout::timeout(@timeout_value) {
+          cap = PacketFu::Capture.new(:iface => @config[:iface], :start => true)
+          cap.stream.each do |p|
+            pkt = PacketFu::Packet.parse p
+            if check_packet_type(pkt)
+              is_successful
+              puts ("czy udp? " + pkt.is_udp?.to_s)
+            elsif check_if_icmp(pkt)
+              if pkt.icmp_type == 3 and (pkt.icmp_code == 0 or 1 or 2 or 9 or 10 or 13)
+                is_filtered
+              elsif pkt.icmp_type == 3 and pkt.icmp_code == 3
+                is_not_successful
+              end
+              break
+            else
+              next
+            end
+          end
+        }
+      rescue Timeout::Error
+        on_timeout
+      end
+    end
+    @tries.times do
+      @prepared_packet.to_w
+      sleep @sleep_time
+    end
+    capture_thread.join
+  end
+
+end
+
+ip = "192.168.0.10"
+dst = 53
+src = 1998
+timeout_value = 10
+tries = 1
+sleep_time = 2
+
+scanner = UDP_scanner.new(ip, dst, src, timeout_value, tries, sleep_time)
+
+# [1, 22, 139, 445].each do |port|
+#   scanner.set_dst_port(port)
+#   scanner.scann
+# end
+
+
+[53, 68, 137, 631].each do |port|
+  scanner.set_dst_port(port)
+  scanner.scann
+end
+
+
